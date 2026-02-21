b2f82d2ab6315d8ec9d291d26701fc9b2e3f7074
CarlosJaeger <carlosjaragebhard@outlook.com>
2026-02-21 13:17:43 -0300

    Initial Commit , promtail + loki + grafana

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+BACKUP_DIR=./backups/$DATE
+
+mkdir -p $BACKUP_DIR
+
+docker run --rm \
+  -v loki_data:/volume \
+  -v $(pwd)/backups/$DATE:/backup \
+  alpine \
+  tar czf /backup/loki.tar.gz -C /volume .
+
+docker run --rm \
+  -v grafana_data:/volume \
+  -v $(pwd)/backups/$DATE:/backup \
+  alpine \
+  tar czf /backup/grafana.tar.gz -C /volume .
